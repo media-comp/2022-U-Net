33017d130dd2b60ec5ef2318cf86e2d17e73a8c7
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2022-05-13 10:44:08 +0900

    Fixed some bugs, workflow should be working now
    - added .gitignore to exclude some temporary files and directories not necessary for the repo
    - updated main.py
        - extend the comment section for excluding training code to the loading of training images, since these are not available in the repository
        - fixed a bug, where glob will return filenames in arbitrary order, and saving them with batch index in the predict function will mess up the order.
            - modified the collect_images function to also keep track of the filenames and return them as a list.
            - use the batch index in write_predicted_images to get the original filename from the list of names.

--- main.py
+++ main.py
@@ -5,49 +5,58 @@ import cv2
 import numpy as np
 from keras.utils.np_utils import to_categorical
 from keras.models import load_model
+from pathlib import Path
 
 n_classes = 4  # Number of classes for segmentation
 
 
 def collect_images(path):
     images_set = []
+
+    # keep track of the filenames, since glob returns filenames in arbitrary order,
+    # saving them later based on batch index messes up the order
+    filenames = []
     for directory_path in glob.glob(path):
         for img_path in glob.glob(os.path.join(directory_path, "*.png")):
             img = cv2.imread(img_path, 1)
             # img = cv2.resize(img, (SIZE_Y, SIZE_X))
             images_set.append(img)
-    return images_set
+            filenames.append(Path(img_path).stem)
+    return images_set, filenames
 
 
-train_images = collect_images("train_image/")
-train_images = np.array(train_images)
-
-train_masks = collect_images("train_semantic/")
-train_masks = np.array(train_masks)
-train_masks = train_masks[:, :, :, 0]
-
-#########data normalization
-train_images = np.expand_dims(train_images, axis=4)
-train_images = train_images / 255
-
-train_masks_input = np.expand_dims(train_masks, axis=3)
-
-y_train = train_masks_input
-X_train = train_images
-
-############make train mask data which have 4 categories to display data in categorical way
-
-train_masks_cat = to_categorical(y_train, num_classes=n_classes)
-y_train_cat = train_masks_cat.reshape((y_train.shape[0], y_train.shape[1], y_train.shape[2], n_classes))
-
-###############################################################
-
-
-IMG_HEIGHT = X_train.shape[1]
-IMG_WIDTH = X_train.shape[2]
-IMG_CHANNELS = X_train.shape[3]
+# Extended the comment section to include reading of training images, since those are not available in the repo
 
 # uncomment this section if you would like to train the network
+
+# train_images, _ = collect_images("train_image/")
+# train_images = np.array(train_images)
+#
+# train_masks, _ = collect_images("train_semantic/")
+# train_masks = np.array(train_masks)
+# train_masks = train_masks[:, :, :, 0]
+#
+# #########data normalization
+# train_images = np.expand_dims(train_images, axis=4)
+# train_images = train_images / 255
+#
+# train_masks_input = np.expand_dims(train_masks, axis=3)
+#
+# y_train = train_masks_input
+# X_train = train_images
+#
+# ############make train mask data which have 4 categories to display data in categorical way
+#
+# train_masks_cat = to_categorical(y_train, num_classes=n_classes)
+# y_train_cat = train_masks_cat.reshape((y_train.shape[0], y_train.shape[1], y_train.shape[2], n_classes))
+#
+# ###############################################################
+#
+#
+# IMG_HEIGHT = X_train.shape[1]
+# IMG_WIDTH = X_train.shape[2]
+# IMG_CHANNELS = X_train.shape[3]
+
 #
 # class Model:
 #
@@ -86,7 +95,7 @@ class Predict:
 
     def __init__(self, model_parameter_file_name, test_image_path):
         self.model_parameter_file_name = model_parameter_file_name
-        self.test_images = collect_images("%s" % test_image_path)
+        self.test_images, self.filenames = collect_images("%s" % test_image_path)
 
     def write_predicted_images(self):
         model_testing = load_model('%s' % self.model_parameter_file_name, compile=False)
@@ -97,6 +106,8 @@ class Predict:
         max = np.argmax(y_pred, axis=3)
         max = max * 40
 
+        os.makedirs('predict', exist_ok=True)       # added this line because imwrite does not create directory
+
         for a in range(0, max.shape[0]):
             gray_img = np.zeros((368, 1232, 3))
             gray_img = gray_img.astype('int8')
@@ -104,7 +115,8 @@ class Predict:
             gray_img[:, :, 1] = max[a] * 2
             gray_img[:, :, 2] = max[a]
 
-            cv2.imwrite('predict/%d.png' % a, gray_img)
+            # instead of using batch index directly, use it to get the filename from the list returned by collect_images
+            cv2.imwrite('predict/%s.png' % self.filenames[a], gray_img)
 
 
 Predict('test7.hdf5', "testing_images/").write_predicted_images()
