2c456c3fd25524652ea63a3a98e6a766b0538c75
japanopenthegate <94523123+japanopenthegate@users.noreply.github.com>
2022-05-13 14:43:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,46 +6,46 @@ import numpy as np
 from keras.utils.np_utils import to_categorical
 from keras.models import load_model
 
-# n_classes = 4  # Number of classes for segmentation
+n_classes = 4  # Number of classes for segmentation
 
 
-# def collect_images(path):
-#     images_set = []
-#     for directory_path in glob.glob(path):
-#         for img_path in glob.glob(os.path.join(directory_path, "*.png")):
-#             img = cv2.imread(img_path, 1)
-#             # img = cv2.resize(img, (SIZE_Y, SIZE_X))
-#             images_set.append(img)
-#     return images_set
+def collect_images(path):
+    images_set = []
+    for directory_path in glob.glob(path):
+        for img_path in glob.glob(os.path.join(directory_path, "*.png")):
+            img = cv2.imread(img_path, 1)
+            # img = cv2.resize(img, (SIZE_Y, SIZE_X))
+            images_set.append(img)
+    return images_set
 
 
-# train_images = collect_images("train_image/")
-# train_images = np.array(train_images)
+train_images = collect_images("train_image/")
+train_images = np.array(train_images)
 
-# train_masks = collect_images("train_semantic/")
-# train_masks = np.array(train_masks)
-# train_masks = train_masks[:, :, :, 0]
+train_masks = collect_images("train_semantic/")
+train_masks = np.array(train_masks)
+train_masks = train_masks[:, :, :, 0]
 
-# #########data normalization
-# train_images = np.expand_dims(train_images, axis=4)
-# train_images = train_images / 255
+#########data normalization
+train_images = np.expand_dims(train_images, axis=4)
+train_images = train_images / 255
 
-# train_masks_input = np.expand_dims(train_masks, axis=3)
+train_masks_input = np.expand_dims(train_masks, axis=3)
 
-# y_train = train_masks_input
-# X_train = train_images
+y_train = train_masks_input
+X_train = train_images
 
-# ############make train mask data which have 4 categories to display data in categorical way
+############make train mask data which have 4 categories to display data in categorical way
 
-# train_masks_cat = to_categorical(y_train, num_classes=n_classes)
-# y_train_cat = train_masks_cat.reshape((y_train.shape[0], y_train.shape[1], y_train.shape[2], n_classes))
+train_masks_cat = to_categorical(y_train, num_classes=n_classes)
+y_train_cat = train_masks_cat.reshape((y_train.shape[0], y_train.shape[1], y_train.shape[2], n_classes))
 
-# ###############################################################
+###############################################################
 
 
-# IMG_HEIGHT = X_train.shape[1]
-# IMG_WIDTH = X_train.shape[2]
-# IMG_CHANNELS = X_train.shape[3]
+IMG_HEIGHT = X_train.shape[1]
+IMG_WIDTH = X_train.shape[2]
+IMG_CHANNELS = X_train.shape[3]
 
 # # uncomment this section if you would like to train the network
 # #
